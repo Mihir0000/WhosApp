0b0d87c45061a8f27fc8b4c02542d98733985c6d
Mihir Baidya <mihirbaidya08@gmail.com>
2022-04-28 13:42:49 +0530

    fix sub

--- components/layout/chatSide.tsx
+++ components/layout/chatSide.tsx
@@ -1,29 +1,77 @@
-import { Fragment, useState, useRef, useEffect } from 'react';
+import { Fragment, useState, useRef, useEffect, useCallback } from 'react';
 import styles from './chatSide.module.css';
 import PublishChat from '../pubSub/PublishChat';
 import axios from 'axios';
 
 function ChatSide({ channel }: any) {
     const chatRef = useRef<any>('');
+    const scrollingRef = useRef<string>('');
     const [channelChat, setChannelChat] = useState([]);
     const [userName, setUserName] = useState<string>('');
     const [chat, setChat] = useState('');
+    const [msg, setMsg] = useState('new');
+
+    const response = useCallback(async () => {
+        const url = `https://api.backendless.com/2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00/7AF7BA66-76AA-4745-9E9B-54E91012A820/data/${channel}?pageSize=100&sortBy=%60time%60%20asc`;
+        const { data } = await axios.get(url);
+        console.log(data);
+        setChannelChat(data);
+        if (data.length > 0) {
+            setMsg(data[data.length - 1].message);
+        }
+    }, [channel]);
+
     useEffect(() => {
         async function userCall() {
             const value = localStorage.getItem(
                 'Backendless_2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00'
             );
-            const singleUserData = JSON.parse(value);
-            const { 'current-user-id': userID } = singleUserData;
-            const url = `https://fastsand.backendless.app/api/data/Users/${userID}`;
-            const { data } = await axios.get(url);
-            // console.log(userID);
-            setUserName(data.email);
-            // console.log(userName);
-            // console.log(data.email);
+            if (value) {
+                const singleUserData = JSON.parse(value);
+                const { 'current-user-id': userID } = singleUserData;
+                const url = `https://api.backendless.com/2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00/7AF7BA66-76AA-4745-9E9B-54E91012A820/data/Users/${userID}`;
+                const { data } = await axios.get(url);
+                // console.log(userID);
+                setUserName(data.email);
+                // console.log(userName);
+                // console.log(data.email);
+            }
         }
+        async function res(){
+            const url = `https://api.backendless.com/2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00/7AF7BA66-76AA-4745-9E9B-54E91012A820/data/${channel}?pageSize=100&sortBy=%60time%60%20asc`;
+            const { data } = await axios.get(url);
+            console.log(data);
+            setChannelChat(data);
+            if (data.length > 0) {
+                setMsg(data[data.length - 1].message);
+            }
+        }
+        const subscribe = () => {
+            var subscribeChannel = Backendless.Messaging.subscribe(channel);
+            function onMessage(subMessage: any) {
+                setMsg(subMessage.message);
+                console.log(subMessage);
+            }
+            subscribeChannel.addMessageListener(onMessage);
+        };
+
+        res();
         userCall();
-    }, []);
+        subscribe();
+    }, [channel]);
+    useEffect(() => {
+        // setTimeout(() => {
+        response();
+        // }, 0);
+    }, [channel, response, msg]);
+
+    useEffect(() => {
+        const scrolling = () => {
+            scrollingRef &&
+                scrollingRef.current.scrollIntoView({ behavior: 'smooth' });
+        };
+        scrolling();
+    }, [channelChat]);
     const submitHandler = (e: any) => {
         e.preventDefault();
         console.log('chat submitted');
@@ -33,17 +81,56 @@ function ChatSide({ channel }: any) {
         const name = userName.split('@');
         PublishChat({ channel, message, pubOptions: { sender: name[0] } });
         setChat('');
+        console.log(channel);
+
+        var subscribeChannel = Backendless.Messaging.subscribe(channel);
+        function onMessage(subMessage: any) {
+            // console.log('Message received: ' + subMessage);
+            // console.log('Message Received');
+            // console.log(subMessage.message);
+            // setSubMsg(subMessage);
+            setMsg(subMessage.message);
+            console.log(subMessage);
+        }
+        subscribeChannel.addMessageListener(onMessage);
     };
+    const readableTime = (time: number) => {
+        const date = new Date(time);
+        return date.toLocaleString();
+    };
+    // setInterval(()=>{
+    //     if (totalChat != channelChat) {
+    //         setTotalChat(channelChat);
+    //     }
+    // },100)
+    // useEffect(() => {
+    //     console.log('Change Whole Chat');
+    // }, [channelChat]);
     return (
         <Fragment>
             <div className={styles.chat_container}>
                 <div className={styles.channel_chat}>
-                    hi {channel}
-                    {/* {
-                        channelChat.map((item:any, index:number)=>(
+                    {channelChat.length === 0 ? (
+                        <div className={styles.no_chat}>No Chat Here</div>
+                    ) : (
+                        channelChat.map((item: any, index: number) => (
+                            <div className={styles.message_box} key={index}>
+                                <div className={styles.chat_top}>
+                                    <div className={styles.sender}>
+                                        From : {item.sender}
+                                    </div>
+                                    <div className={styles.time}>
+                                        {readableTime(item.time)}
+                                    </div>
+                                </div>
 
+                                <div className={styles.message}>
+                                    {item.message}
+                                </div>
+                            </div>
                         ))
-                    } */}
+                    )}
+                    <div ref={scrollingRef} />
                 </div>
                 <form className={styles.chat_form} onSubmit={submitHandler}>
                     <input
@@ -58,7 +145,7 @@ function ChatSide({ channel }: any) {
                     />
                     <button
                         type="submit"
-                        onClick={(e) => console.log('clicked')}
+                        // onClick={(e) => console.log('clicked')}
                         className={styles.send_btn}
                     >
                         send me
--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -5,37 +5,39 @@ import ChatSide from '../../components/layout/chatSide';
 import axios from 'axios';
 
 function ChatPage() {
-    const [channel, setChannel] = useState('First_Channel');
+    const [channel, setChannel] = useState('Channel_I');
     const [userName, setUserName] = useState('');
     useEffect(() => {
         async function userCall() {
             const value = localStorage.getItem(
                 'Backendless_2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00'
             );
-            const singleUserData = JSON.parse(value);
-            const { 'current-user-id': userID } = singleUserData;
-            const url = `https://fastsand.backendless.app/api/data/Users/${userID}`;
-            const { data } = await axios.get(url);
-            // console.log(userID);
-            setUserName(data.email);
-            // console.log(userName);
-            // console.log(data.email);
+            if (value) {
+                const singleUserData = JSON.parse(value);
+                const { 'current-user-id': userID } = singleUserData;
+                const url = `https://api.backendless.com/2C1B1F9E-7BEE-C020-FF8D-B4A820E4DB00/7AF7BA66-76AA-4745-9E9B-54E91012A820/data/Users/${userID}`;
+                const { data } = await axios.get(url);
+                // console.log(userID);
+                setUserName(data.email);
+                // console.log(userName);
+                // console.log(data.email);
+            }
         }
         userCall();
     }, []);
     // console.log(userName);
 
     const channelData = [
-        { channel: 'First_Channel' },
-        { channel: 'Second_Channel' },
-        { channel: 'Third_Channel' },
-        { channel: '4th_Channel' },
-        { channel: '5th_Channel' },
-        { channel: '6th_Channel' },
-        { channel: '7th_Channel' },
-        { channel: '8th_Channel' },
-        { channel: '9th_Channel' },
-        { channel: '10th_Channel' },
+        { channel: 'Channel_I' },
+        { channel: 'Channel_II' },
+        { channel: 'Channel_III' },
+        { channel: 'Channel_IV' },
+        { channel: 'Channel_V' },
+        { channel: 'Channel_VI' },
+        { channel: 'Channel_VII' },
+        { channel: 'Channel_VIII' },
+        { channel: 'Channel_IX' },
+        { channel: 'Channel_X' },
     ];
     return (
         <Fragment>
@@ -65,7 +67,7 @@ function ChatPage() {
                     </div>
                 </div>
                 <div className={styles.right}>
-                    <div className={styles.chat_top}>Chat</div>
+                    <div className={styles.chat_top}>{channel}</div>
                     <div className={styles.chat_down}>
                         <ChatSide channel={channel} />
                     </div>
